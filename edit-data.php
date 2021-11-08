f9ffdde58e10dd02909da31e717a950668dbd86e
layleh <93070201+layleh@users.noreply.github.com>
2021-11-08 10:25:46 +0700

    mengubah tanggal otomatis pada berakhir dan baru
    Notice: Undefined variable: line 93
    Notice: Trying to get property 'berakhir' line 93
    Notice: Undefined variable line 99
    Notice: Trying to get property 'baru'  line 99

--- edit-data.php
+++ edit-data.php
@@ -5,18 +5,18 @@
         echo '<script>window.location="login.php"</script>';
     }
 
-    $kode_alat = $_GET['kode_sn'];
-    $data_karyawan = mysqli_query($conn, "SELECT * FROM tb_alat WHERE nip = '".$kode_alat."' ");
+    $kode = $_GET['kode'];
+    $data_alat = mysqli_query($conn, "SELECT * FROM tb_alat WHERE kode = '".$kode."' ");
 
-    $p = mysqli_fetch_object($data_karyawan);
+    $p = mysqli_fetch_object($data_alat);
 
     if(isset($_POST['update'])){
-        $query = "UPDATE `tb_pendataan` SET nama='$_POST[nama]', jabatan='$_POST[jabatan]', alamat='$_POST[alamat]', no_hp='$_POST[no_hp]', email='$_POST[email]' WHERE nip='$nip_data'";
+        $query = "UPDATE `tb_alat` SET nama='$_POST[nama]', jenis='$_POST[jenis]', berakhir='$_POST[berakhir]', baru='$_POST[baru]', WHERE kode_data='$kode'";
         $query_run = mysqli_query($conn, $query);
 
         if($query_run){
             echo '<script type="text/javascript"> alert("Data Update") </script>';
-            echo '<script>window.location="data-karyawan.php"</script>';
+            echo '<script>window.location="data-asset.php"</script>';
         }
         else{
             echo '<script type="text/javascript"> alert("Data Not Update") </script>';
@@ -29,7 +29,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Form Pendataaan Karyawan | Administrator </title>
+        <title> Form Controlling Asset | Administrator </title>
         <link rel="stylesheet" type="text/css" href="css/editPage.css">
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
         rel="stylesheet">
@@ -37,10 +37,10 @@
     <body>
         <!-- bagian header -->
         <header>
-            <h1><a href="beranda.php"> Admin Pendataan Karyawan </a></h1>
+            <h1><a href="beranda.php"> Admin Controlling Asset </a></h1>
             <ul>
                 <li><a href="beranda.php">Beranda</a></li>
-                <li><a href="data-karyawan.php">Data Karyawan</a></li>
+                <li><a href="data-asset.php">Data Asset</a></li>
                 <li<a href="#input-data.php">Input Data Baru</a></li>
                 <li><a href="keluar.php">Keluar</a></li>
                 
@@ -49,53 +49,56 @@
 
         <!-- bagian content -->
         <section class="content">
-            <h2>Edit Data Karyawan</h2>
+            <h2>Edit Data Alat</h2>
             <div class="box">
-                <table class ="table-data" borde="0">
+                <table class ="table-data" border="0">
                     <tr>
-                        <td>Nama Lengkap</td>
+                        <td>Nama Alat</td>
                         <td>: </td>
                         <td></td>
                         <td><?php echo $p->nama ?></td>
                     </tr><br>
                     <tr>
-                        <td>NIP</td>
+                        <td>Serial No.</td>
                         <td>: </td>
                         <td></td>
-                        <td><?php echo $p->nip ?></td>
+                        <td><?php echo $p->kode ?></td>
                     </tr>
                     <tr>
-                        <td>Jabatan</td>
+                        <td>Jenis Alat</td>
                         <td>: </td>
                         <td></td>
-                        <td><?php echo $p->jabatan ?></td>
-                    <tr>
-                        <td>Tempat Tinggal</td>
-                        <td>: </td>
-                        <td></td>
-                        <td><?php echo $p->alamat ?></td>
+                        <td><?php echo $p->jenis ?></td>
                     </tr>
                     <tr>
-                        <td>No. HP/WA</td>
+                        <td>Kalibrasi Terakhir</td>
                         <td>: </td>
                         <td></td>
-                        <td><?php echo $p->no_hp ?></td>
-                    </tr>
+                        <td><?php echo $p->berakhir ?></td>
                     <tr>
-                        <td>Email</td>
+                        <td>Kalibrasi Selanjutnya</td>
                         <td>: </td>
                         <td></td>
-                        <td><?php echo $p->email ?></td>
+                        <td><?php echo $p->baru ?></td>
                     </tr>
                 </table>
                 <br>
                 <center>
                     <form action="" method="POST">
-                        <input type="text" name="nama" placeholder="Masukkan Nama"/><br>
-                        <input type="text" name="jabatan" placeholder="Masukkan Jabatan"/><br>
-                        <input type="text" name="alamat" placeholder="Masukkan Alamat"/><br>
-                        <input type="text" name="no_hp" placeholder="Masukkan No HP"/><br>
-                        <input type="text" name="email" placeholder="Masukkan Email"/><br>
+                        <input type="text" name="alat" placeholder="Masukkan Nama Alat"/><br>
+                        <input type="text" name="kode" placeholder="Masukkan Serial No" /><br>
+                        <input type="text" name="jenis" placeholder="Masukkan Jenis Alat"/><br>
+                    <div class="form-group">
+                            <label>Tanggal Berakhir</label>
+                            <?php $date = date_create($data_edit->berakhir); ?>
+                            <input type="datetime-local" name="berakhir" class="form-control" min="<?= date_format($date, 'Y-m-d\TH:i'); ?>" value='<?= date_format($date, 'Y-m-d\TH:i'); ?>' required>  
+                    </div>
+
+                    <div class="form-group">
+                            <label>Tanggal Selanjutnya</label>
+                            <?php $date = date_create($data_edit->baru); ?>
+                            <input type="datetime-local" name="baru" min="<?= date_format($date, 'Y-m-d\TH:i'); ?>" class="form-control" value='<?= date_format($date, 'Y-m-d\TH:i'); ?>' required> 
+                    </div>
 
                         <input type="submit" name="update" value="UPDATE DATA" class="button"/>
                     </form>
