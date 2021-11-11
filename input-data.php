5d17345dd4c840b63906b3667184c0064964f93e
Fathul <61180722+basyair7@users.noreply.github.com>
2021-11-11 17:29:32 +0700

    update baru, semua baris
    program awalnya

--- input-data.php
+++ input-data.php
@@ -4,6 +4,30 @@
     if($_SESSION['stat_login'] != true){
         echo '<script>window.location="login.php"</script>';
     }
+
+    if(isset($_POST['submit'])){
+        $kode = $_POST['kode'];
+        $nama = $_POST['nama'];
+        $jenis = $_POST['jenis'];
+        $berakhir = $_POST['berakhir'];
+        $baru = $_POST['baru'];
+
+        if(empty($nama)){
+            echo '<script type="text/javascript"> alert("Nama Tidak Boleh Kosong") </script>';
+        }
+        elseif(empty($kode)){
+            echo '<script type="text/javascript"> alert("Kode Tidak Boleh Kosong") </script>';
+        }
+        elseif(empty($jenis)){
+            echo '<script type="text/javascript"> alert("Jenis Tidak Boleh Kosong") </script>';
+        }
+        elseif(empty($berakhir)){
+            echo '<script type="text/javascript"> alert("Tanggal Berakhir Tidak Boleh Kosong") </script>';
+        }
+        elseif(empty($baru)){
+            echo '<script type="text/javascript"> alert("Tanggal Baru Tidak Boleh Kosong") </script>';
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +35,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Controlling Asset | Administrator </title>
-        <link rel="stylesheet" type="text/css" href="css/beranda.css">
+        <title> Form Controlling Asset | Administrator </title>
+        <link rel="stylesheet" type="text/css" href="css/editPage.css">
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
         rel="stylesheet">
     </head>
@@ -23,219 +47,30 @@
             <ul>
                 <li><a href="beranda.php">Beranda</a></li>
                 <li><a href="data-asset.php">Data Asset</a></li>
-                <li><a href="input-data.php">Input Data Baru</a></li>
                 <li><a href="keluar.php">Keluar</a></li>
                 
             </ul>
         </header>
-<?php
-    include 'koneksi.php';
-    
-    // cek jika input kosong
 
-    if(isset($_POST['submit'])){
-        // membuat variabel
-        $kode = $_POST['kode_data'];
-        $nama = $_POST['nama'];
-        $jenis = $_POST['jenis'];
-        $berakhir = $_POST['berakhir'];
-        $baru = $_POST['baru'];
-
-        // cek jika input kosong, maka muncul error
-        if(empty($nama)){
-            header("location:index.php?errnama= * Nama Belum di Input");
-        }
-        elseif(empty($kode)){
-            header("location:index.php?errkode_data= * Serial No. Belum di Input");
-        }
-        elseif(empty($jenis)){
-            header("location:index.php?errjenis= * Jenis Alat Belum di Input");
-        }
-        elseif(empty($berakhir)){
-            header("location:index.php?errberakhir= * Kalibrasi Berakhir Belum di Input");
-        }
-        elseif(empty($baru)){
-            header("location:index.php?errbaru= * Silahkan Pilih");
-        }
-        else{
-            // proses insert
-            $insert = mysqli_query($conn, "INSERT INTO tb_alat VALUES (
-                '".$kode."',
-                '".$nama."',
-                '".$jenis."',
-                '".$berakhir."',
-                '".$baru."',
-            )");
-            $insert=mysqli_query($conn);
-
-//Kondisi apakah berhasil atau tidak
-  if ($insert) {
-    echo "Berhasil Input";
-  }
-else {
-    echo "Gagal Input";
-}
-        }
-    }
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Input Data Baru </title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
-        rel="stylesheet">
-    </head>
-    <body>
-    <header>
-        
-    </header>
-        <!-- Bagian box formulir -->
-        <section class="box-formulir">
-        <div class="box-title">
-            <br>
-            <h1>
-                <p align="center">
-                    Input Data Baru 
-                </p>
-            </h1>
-        </div>
-        <br>
-
-            <!-- bagian form -->
-            <form action="" method="post">
-                <div class="box">
-                    
-                    <table border="0" class="table-form">
-                        <h4 class="error">
-                         
-                        </h4>
+        <!-- bagian content -->
+        <section class="content">
+            <h2>Input Data Alat Baru</h2>
+            <div class="box">
+                <center>
+                    <form action="" method="POST">
+                        <pre class="nama-alat"> Nama Alat : </pre>
+                        <input type="text" name="alat" placeholder="Masukkan Nama Alat"/>
                         <br>
-                        
-                        <tr>
-                            <td>Nama Alat</td>
-                            <td>
-                                <p align="left">: </p> 
-                            </td>
-                            <td>
-                                <input type="text" name="nama" class="input-control">
-                            </td>
-                            <td class="error">
-                                <?php
-                                if(isset($_GET["errnama"])){
-                                    $err = $_GET["errnama"];
-                                    echo $err;
-                                }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <tr></tr>
-                        <tr></tr>
-
-                        <tr>
-                            <td>Serial No</td>
-                            <td>
-                                <p align="left">: </p> 
-                            </td>
-                            <td>
-                                <input type="text" name="kode_data" class="input-control">
-                            </td>
-                            <td class="error">
-                                <?php
-                                if(isset($_GET["errkode_data"])){
-                                    $err = $_GET["errkode_data"];
-                                    echo $err;
-                                }
-                                ?>
-                            </td>
-                        </tr>
-
-                        <tr></tr>
-                        <tr></tr>
-
-                        <tr>
-                            <td>Jenis Alat</td>
-                            <td>
-                                <p align="left">: </p> 
-                            </td>
-                            <td>
-                                <input type="text" name="jenis" class="input-control">
-                            </td>
-                            <td class="error">
-                                <?php
-                                if(isset($_GET["errjenis"])){
-                                    $err = $_GET["errjenis"];
-                                    echo $err;
-                                }
-                                ?>
-                            </td>
-                        </tr>
-
-                        <tr></tr>
-                        <tr></tr>
-                        
-                        <tr>
-                            <td>Kalibrasi Alat Berakhir</td>
-                            <td>
-                                <p align="left">: </p> 
-                            </td>
-                            <td>
-                                <input type="date" name="berkahir" class="input-control">
-                            </td>
-                            <td class="error">
-                                <?php
-                                if(isset($_GET["errberakhir"])){
-                                    $err = $_GET["errberakhir"];
-                                    echo $err;
-                                }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <tr></tr>
-                        <tr></tr>
-                        
-                        <tr>
-                            <td>Kalibrasi Selanjutnya</td>
-                            <td>
-                                <p align="left">: </p> 
-                            </td>
-                            <td>
-                                <input type="date" name="baru" class="input-control">
-                            </td>
-                            <td class="error">
-                                <?php
-                                if(isset($_GET["errbaru"])){
-                                    $err = $_GET["errbaru"];
-                                    echo $err;
-                                }
-                                ?>
-                            </td>
-                        </tr>
-
-                        <tr></tr>
-                        <tr></tr>                        
-                        <tr></tr>
-                        <tr></tr>
-                        <tr></tr>
-                        <tr></tr>
-                        
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <input type="submit" name="submit" class="btn-daftar" value="Upload Data">
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </form>
-        
+                        <td>Serial No :</td>
+                        <input type="text" name="kode" placeholder="Masukkan Serial No" /><br>
+                        <td>Jenis Alat :</td>
+                        <input type="text" name="jenis" placeholder="Masukkan Jenis Alat"/><br>
+                        <input type="date" name="berakhir" placeholder="Masukkan Tanggal Kalibrasi Berakhir" class="input-control"><br>
+                        <input type="date" name="baru" placeholder="Masukkan Tanggal Kalibrasi Baru" class="input-control"><br>
+                        <input type="submit" name="update" value="INPUT DATA" class="button-input"/>
+                    </form>
+                </center>
+            </div>
         </section>
-
     </body>
 </html>
