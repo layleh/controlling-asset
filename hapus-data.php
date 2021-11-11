2f59b21d016a9cf0da949e40151c7fa9184cdd58
Fathul <61180722+basyair7@users.noreply.github.com>
2021-11-11 18:07:07 +0700

    Update Baru

--- hapus-data.php
+++ hapus-data.php
@@ -1,9 +1,9 @@
 <?php
     include 'koneksi.php';
 
-    if(isset($_GET['kode_sn'])){
-        $kode_alat = $_GET['kode_sn'];
-        $delete = mysqli_query($conn, "DELETE FROM tb_alat WHERE nip = '".$kode_alat."' ");
+    if(isset($_GET['kode'])){
+        $kode_alat = $_GET['kode'];
+        $delete = mysqli_query($conn, "DELETE FROM tb_alat WHERE kode = '".$kode_alat."' ");
 
         echo '<script>window.location="data-asset.php"</script>';
     }
